7dc219749c24eef2c617e563b98de808f34ddb8f
Leigh Halliday <leighhalliday@gmail.com>
2020-03-30 21:21:28 -0400

    Fixing the against property and adding tsvector_column option

--- app/models/job.rb
+++ app/models/job.rb
@@ -2,6 +2,10 @@ class Job < ApplicationRecord
   include PgSearch::Model
   pg_search_scope :search_title, against: :title
   pg_search_scope :search_job,
-                  against: [{ title: 'A', description: 'B' }],
-                  using: { tsearch: { dictionary: 'english' } }
+                  against: { title: 'A', description: 'B' },
+                  using: {
+                    tsearch: {
+                      dictionary: 'english', tsvector_column: 'searchable'
+                    }
+                  }
 end
